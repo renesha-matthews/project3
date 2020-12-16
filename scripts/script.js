9cf4310c8a9785f5a57e4c016d27d84875d0cdcb
Renesha Matthews <rmatthews1@ufl.edu>
2020-12-16 15:57:04 -0500

    added styling to portfolio page

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-  $('.slider').slick({
-    
-  });
+$('.slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
 });
