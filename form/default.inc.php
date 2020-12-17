efeca8199b189b15faad3e56651a5a96487523a8
Renesha Matthews <rmatthews1@ufl.edu>
2020-12-17 16:56:33 -0500

    added css to php files

--- form/default.inc.php
+++ form/default.inc.php
@@ -1,3 +1,124 @@
+<style type="text/css">
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+html {
+  min-height: 100%;
+}
+
+.container {
+  max-width: 1500px;
+  margin: 0 auto;
+}
+
+body {
+  background-image: linear-gradient(to bottom right, #fdfbfb, #ebedee);
+  background-repeat: no-repeat;
+  color: #111111;
+  font: 16px 'Inter', sans-serif;
+}
+
+h1 {
+  font-size: 72px;
+  padding: 30px;
+  text-align: center;
+}
+
+h2 {
+  font-size: 38px;
+}
+
+h3 {
+  font-size: 24px;
+}
+
+h1,
+h2,
+h3 {
+  font-family: 'Fira Code', monospace;
+}
+
+h1::selection,
+h2::selection,
+h3::selection,
+p::selection {
+  background: #ffb696;
+  color: #fff;
+}
+
+header {
+  text-align: center;
+  padding: 50px 0;
+}
+
+.logo-container {
+  position: relative;
+  float: left;
+  margin-left: 40px;
+  transition: .3s;
+}
+
+.logo-container:hover {
+  opacity: 0.5;
+  transition: .3s;
+}
+
+
+nav ul li {
+  display: inline-block;
+  font: 18px 'Inter', sans-serif;
+  padding: 30px;
+}
+
+nav ul li a {
+  color: #111111;
+  text-decoration: none;
+  text-transform: uppercase;
+  transition: .3s;
+}
+
+nav ul li a:hover {
+  color: #ff7a95;
+  transition: .3s;
+}
+
+nav ul li a:active {
+  color: #ffb696;
+}
+
+footer {
+    clear: both;
+    font-weight: bold;
+    text-align: center;
+    margin: 50px;
+}
+
+.connect {
+    text-transform: uppercase;
+    margin: 12px 0;
+}
+
+.socialmedia a {
+    font-size: 50px;
+    color: #ff7a95;
+    margin: 5px;
+    transition: .3s;
+}
+
+.socialmedia a:hover {
+  color: #ffb696;
+  transition: .3s;
+}
+
+.site-credit {
+  font-size: 14px;
+  margin-top: 8px;
+}
+</style>
+
 <header>
   <nav>
     <ul>
@@ -15,11 +136,11 @@
 </div>
 
 <footer>
-  <p>Let's Connect!</p>
-  <p>
-    <a href="https://www.instagram.com/reneshadotcom/"><i class="fab fa-instagram-square"></i></a>
-    <a href="https:///www.twitter.com/reneshadotcom"><i class="fab fa-twitter-square"></i></a>
-    <a href="https://www.linkedin.com/in/renesha-matthews-903b24126/"><i class="fab fa-linkedin"></i></a>
+  <p class="connect">Let's Connect!</p>
+  <p class="socialmedia">
+    <a href="https://www.instagram.com/reneshadotcom/" target="_blank"><i class="fab fa-instagram-square"></i></a>
+    <a href="https:///www.twitter.com/reneshadotcom" target="_blank"><i class="fab fa-twitter-square"></i></a>
+    <a href="https://www.linkedin.com/in/renesha-matthews-903b24126/" target="_blank"><i class="fab fa-linkedin"></i></a>
   </p>
-  <p>&copy; 2020 Renesha Matthews.</p>
+  <p class="site-credit">Coded and Designed by: Renesha Matthews</p>
 </footer>
